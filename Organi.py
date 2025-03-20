e15ea79248ad039aba37f0224104e1b4e0cf50b8
madarakaleja <madara.kaleja@grobinasskola.lv>
2025-03-20 09:45:16 +0200

    Init

--- Organi.py
+++ Organi.py
@@ -1,1 +1,20 @@
 import PySimpleGUI as org
+ 
+ 
+# Izkārtojuma definīcija
+saraksts=[['File',['Close']], ['Help'],['About']]
+organs = [
+        [org.Menu(saraksts)],
+        
+     ]  
+logs = org.Window("ORGĀNU SPĒLE", organs, size=(900,650))
+
+
+while True:
+    event, values = logs.read()
+ 
+    # Kā IZET, ja logs tiek aizvērts
+    if event == org.WINDOW_CLOSED or event=='Close':
+       break
+    elif event=='About':
+        org.popup('Autori: Emīls Ronis un Evelīna Ģelze')

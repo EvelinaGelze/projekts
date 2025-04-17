ee518e5ade763fb10cd64b86b418466df0a3ab19
madarakaleja <madara.kaleja@grobinasskola.lv>
2025-04-17 09:27:29 +0300

    Jautājumi

--- Organi.py
+++ Organi.py
@@ -4,6 +4,29 @@ from tkinter import messagebox
  
 # Izkārtojuma definīcija
 saraksts=[['File',['Close']], ['Help'],['About']]
+
+jautajumi = [
+    {"jautajums": "Redzes orgāns?", 
+     "atbildes": ["acis", "sirds", "plaušas"],
+     "pareizā_atbilde": "acis"},
+
+    {"jautajums": "Bez šī orgāna tavs ķermenis nespētu attīrīt asinis un izvadīt toksīnus.", 
+     "atbildes": ["Nieres", "Sirds", "Aknas"], 
+     "pareizā_atbilde": "Aknas"},
+
+    {"jautajums": "Kurš ir cilvēka lielākais orgāns?",
+     "atbildes": ["Āda", "Sirds", "Acis"], 
+     "pareizā_atbilde": "Āda"},
+
+    {"jautajums": "Kurš orgāns sūknē asinis pa visu ķermeni?", 
+     "atbildes": ["Plaušas", "Sirds", "Aknas"], 
+     "pareizā_atbilde": "Sirds"},
+
+    {"jautajums": "Kurš orgāns veic elpošanu?", 
+     "atbildes": ["Plaušas", "Deguns", "Smadzenes"], 
+     "pareizā_atbilde": "Plaušas"},
+]
+
 organs = [
         [org.Menu(saraksts)],
         [org.Text("Ar ko cilvēks redz?")],
